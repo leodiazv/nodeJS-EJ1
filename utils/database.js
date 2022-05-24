2d3feb113acc8ea79e84849cb1ad8b5f108a640a
leodiazv <leodiazv@unal.edu.co>
2022-05-24 12:02:40 -0500

    Fixed db configuration

--- utils/database.js
+++ utils/database.js
@@ -11,7 +11,7 @@ const db = new Sequelize({
   database: process.env.DB,
   logging: false,
   dialectOptions:
-    process.env.Node_ENV === 'production'
+    process.env.NODE_ENV === 'production'
       ? {
           ssl: {
             require: true,
